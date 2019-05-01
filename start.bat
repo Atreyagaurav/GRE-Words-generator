37faafab31b5775b2009b28644d574d03080aad0
Gaurav Atreya <atreygaurv98@gmail.com>
2019-05-01 21:33:03 +0545

    i just ended up being lazy. so adding this batch file, which does the initialization for the two different executables needed for the successful run. the program "DesktopAdd1" has to be shutdown from taskbar or task manager though. I'll complete this project when i have time,, right now i have lots of vocabs to remember.

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+color a
+title "GRE Words Generator Initialization"
+Dir WordList\*.txt
+set /p name="Enter the name of the file inside the WordList folder:"
+set /p delay="Enter Delay in seconds:"
+set name=Wordlist\%name%
+start "" "Generate.exe" %name% %delay%
+start "" "DesktopApp1.exe"
