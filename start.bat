af839c5eb65929c6640fc7af0ec65e04eddc3133
Gaurav Atreya <atreygaurv98@gmail.com>
2019-07-12 20:41:49 +0545

    No need to add extension in batch input.

--- start.bat
+++ start.bat
@@ -4,6 +4,6 @@ title "GRE Words Generator Initialization"
 Dir WordList\*.txt
 set /p name="Enter the name of the file inside the WordList folder:"
 set /p delay="Enter Delay in seconds:"
-set name=Wordlist\%name%
+set name=Wordlist\%name%.txt
 start "" "Generate.exe" %name% %delay%
 start "" "DesktopApp1.exe"
